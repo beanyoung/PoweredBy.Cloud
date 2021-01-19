7c48bd5dcb12508837133be34a5e2137137b000f
Bingyu Chen <bingyu.chen@datavisor.com>
2021-01-19 13:37:23 +0800

    Init

---
+++ flask-vue/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, g
+
+
+app = Flask(__name__)
+
+
+sites = [
+    dict(id=1, name='foo'),
+    dict(id=2, name='bar'),
+    dict(id=3, name='baz'),
+    dict(id=4, name='qux'),
+]
+
+
+@app.route('/')
+def index():
+    g.user = 'Tommy'
+    return render_template('index.html', sites=sites)
+
+
+@app.route('/api/sites', methods=['POST'])
+def create_site():
+    site_name = request.get_json()['name']
+    site = dict(id=len(sites) + 1, name=site_name)
+    sites.append(site)
+    return site
+
+
+if __name__ == '__main__':
+    app.run()
